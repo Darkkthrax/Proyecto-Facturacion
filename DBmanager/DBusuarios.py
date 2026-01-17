9cca1b1afdbe4b81606af03c83e69caa649940d6
Darkkthrax <jorozcourrego@gmail.com>
2026-01-16 23:46:57 -0500

    fix conditions in function to create a user
    The order of the conditions was changed and the conditions of the password were fixed

--- DBmanager/DBusuarios.py
+++ DBmanager/DBusuarios.py
@@ -33,18 +33,18 @@ def registrar_usuario_db(root, documento, nombre, apellidos, telefono, correo, c
     from auth.auth import iniciar_sesion
     from gui.facturacion import verificar_entradas_registro
     try:
-        if '@' not in correo:
-            messagebox.showerror("Registro", "Agregue un correo disponible", parent=ventana)
-        elif verificar_entradas_registro(documento, nombre, apellidos, telefono, correo, contrasena, tipo_usuario):
-                messagebox.showwarning("Campos vacíos", "Verifique que todos los campos esté llenos.", parent=ventana)
+        if verificar_entradas_registro(documento, nombre, apellidos, telefono, correo, contrasena, tipo_usuario):
+            messagebox.showwarning("Campos vacíos", "Verifique que todos los campos esté llenos.", parent=ventana)
         elif verificar_usuario_db(documento):
-                messagebox.showwarning("Usuario duplicado", "Ya existe un usuario con el mismo documento. Modifique el documento", parent=ventana)
-        elif len(contrasena) < 8 and contrasena != '':
+            messagebox.showwarning("Usuario duplicado", "Ya existe un usuario con el mismo documento. Modifique el documento", parent=ventana)
+        elif '@' not in correo:
+            messagebox.showerror("Registro", "Agregue un correo disponible", parent=ventana)
+        elif tipo_usuario != 2 and (contrasena == 'Dejar vacío si es cliente' or len(contrasena) < 8):
             messagebox.showwarning("Digitos de contraseña", "Ingrese una contraseña de al menos 8 dígitos")
         else:
             with sqlite3.connect("db/database.db") as conn:
                 cursor = conn.cursor()
-                cursor.execute(f"INSERT INTO tbl_usuarios (documento_identidad, nombre, apellidos, contrasena, telefono, correo, tipo_usuario) VALUES ({documento}, '{nombre}', '{apellidos}', '{encriptar_contrasena(contrasena) if contrasena != 'Dejar vacío si es cliente' else ''}', {telefono}, '{correo}', '{tipo_usuario}')")
+                cursor.execute(f"INSERT INTO tbl_usuarios (documento_identidad, nombre, apellidos, contrasena, telefono, correo, tipo_usuario) VALUES ({documento}, '{nombre}', '{apellidos}', '{encriptar_contrasena(contrasena) if contrasena != '' else ''}', {telefono}, '{correo}', '{tipo_usuario}')")
             messagebox.showinfo("Base de datos", "Usuario agregado correctamente", parent=ventana)
             ventana.destroy()
             if not get_sesion(): iniciar_sesion(root)
