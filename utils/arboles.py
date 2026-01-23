b8fa7f739d03a642946c29ae87679536db18aafd
Darkkthrax <jorozcourrego@gmail.com>
2026-01-22 21:15:25 -0500

    Change function 'on_tree_select'
    Added the parameter 'entry' to change an entry object if there is a selected object in a Treeview (it's not mandatory to fill this parameter every time that will be used the function). The entry object will have a grey placeholder with the selected object's name and will be disabled if there is not a selected object in the Treeview

--- utils/arboles.py
+++ utils/arboles.py
@@ -1,14 +1,21 @@
+import tkinter as tk
 #! FUNCIONES PARA SELECCIONES DE ÁRBOLES
-def on_tree_select(event, tabla, btn1, btn2= None):
+def on_tree_select(event, tabla, btn1, btn2= None, entry = None):
     producto_seleccionado = tabla.focus()
     
     if producto_seleccionado:
         if btn2 != None:
             btn2.config(state='normal')
+        if entry != None:
+            entry.delete(0, tk.END)
+            entry.config(state='normal', fg='grey')
+            entry.insert(0, tabla.item(producto_seleccionado, 'values'))
         btn1.config(state='normal')
     else:
         if btn2 != None:
             btn2.config(state='disabled')
+        if entry != None:
+            entry.config(state='disabled')
         btn1.config(state='disabled')
 
 def on_mousewheel(event, treeview):
