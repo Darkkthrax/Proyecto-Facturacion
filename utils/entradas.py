966d922e6643961aef0db8805232fbebcc97a7b1
Darkkthrax <jorozcourrego@gmail.com>
2026-01-22 21:02:41 -0500

    Add new on_focus parameter
    Added the parameter 'tabla' to add a Treeview object (is not mandatory to fill this parameter) into the functions 'on_focus_in' and 'on_focus_out'. The functions now validate if there is a Treeview that changes an entry object

--- utils/entradas.py
+++ utils/entradas.py
@@ -2,12 +2,23 @@ import tkinter as tk
 
 #! FUNCIONES PARA ENTRADAS
 # Función para crear un placeholder
-def on_focus_in(entry, placeholder):
+def on_focus_in(entry, placeholder, tabla = None):
+    if tabla != None and tabla.selection():
+        if entry.get() == tabla.item(tabla.selection()[0], 'values')[0]:
+            entry.delete(0, tk.END)
+            entry.config(fg='black')
+            return
+        
     if entry.get() == placeholder:
         entry.delete(0, tk.END)
         entry.config(fg='black') # Color normal para texto
 
-def on_focus_out(entry, placeholder):
+def on_focus_out(entry, placeholder, tabla = None):
+    if tabla != None and tabla.selection():
+        if entry.get() == '':
+            entry.insert(0, tabla.item(tabla.selection()[0], 'values'))
+            entry.config(fg='grey')
+            return
     if entry.get() == "":
         entry.insert(0, placeholder)
         entry.config(fg='grey') # Color gris para el placeholder
