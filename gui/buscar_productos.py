e281cda8dd668c655aa50f9d5fda9e15a55aea1f
Darkkthrax <jorozcourrego@gmail.com>
2026-02-12 14:54:46 -0500

    feat(WIP): Add new function to select button
    1. Adds new imports:
    	- Import messagebox from tkinter
    	- Import 'get_productos_factura' and 'set_productos_factura' functions from 'models.models'
    2. Adds new 'tabla_factura' parameter to update billing table
    3. Adds new 'cantidad_producto' entry to insert the quantity of the product to sell
    4. Adds new 'seleccionar_producto' function to select a product from the select product table:
    	- 'ventana': Function parent window
    	- 'tabla': Select product table
    	- 'tabla_factura': Billing table
    	- 'entrada_producto': Search bar entry
    	- 'entrada_cantidad': Product quantity entry
    
    Updates automatically the billing table with selected product and saves its quantity and subtotal price. Also can restart the search bar entry and the select product table after select a product.

--- gui/buscar_productos.py
+++ gui/buscar_productos.py
@@ -1,11 +1,13 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 from utils.entradas import on_focus_in, on_focus_out
 from utils.arboles import *
 from DBmanager.DBproductos import traer_productos_busqueda_db
+from models.models import get_productos_factura, set_productos_factura
 
 # Función para crear la vista para buscar productos
-def buscar_producto(root):
+def buscar_producto(root, tabla_factura):
     headers = ['ID', 'Codigo', 'Nombre', 'Descripción', 'Marca', 'Cantidad Venta', 'Precio']
     ventana_buscador = tk.Toplevel(root)
     ventana_buscador.title('Buscar Producto')
@@ -19,8 +21,16 @@ def buscar_producto(root):
     
     ttk.Label(frame_opciones, text='Buscar Producto', font=("Arial", 16, "bold")).grid(row=0, column=1, padx=5, pady=20, sticky="nw")
     
-    btn_seleccionar = tk.Button(frame_opciones, text='Seleccionar', state='disabled')
-    btn_seleccionar.grid(row=0, column=2, padx=5, pady=5, sticky='nse')
+    ttk.Label(frame_opciones, text='Cantidad:').grid(row=0, column=2, padx=5, pady=5, )
+    
+    cantidad_producto = tk.Entry(frame_opciones, fg='grey')
+    cantidad_producto.grid(row=0, column=3, padx=5, pady=5, sticky='ns')
+    cantidad_producto.insert(0, '1')
+    cantidad_producto.bind("<FocusIn>", lambda event: on_focus_in(cantidad_producto, "1"))
+    cantidad_producto.bind("<FocusOut>", lambda event: on_focus_out(cantidad_producto, "1"))
+    
+    btn_seleccionar = tk.Button(frame_opciones, text='Seleccionar', state='disabled', command= lambda: seleccionar_producto(ventana_buscador, tabla_resultados, tabla_factura, buscador_producto, cantidad_producto))
+    btn_seleccionar.grid(row=0, column=4, padx=5, pady=5, sticky='nse')
     
     buscador_producto = tk.Entry(ventana_buscador)
     buscador_producto.bind('<KeyRelease>', lambda e: retrasar_funcion(ventana_buscador, tabla_resultados, buscador_producto))
@@ -73,3 +83,37 @@ def retrasar_funcion(ventana, tabla, entrada):
         entrada.after_cancel(retrasar_funcion.timer)
     
     retrasar_funcion.timer = entrada.after(700, lambda: actualizar_busqueda(ventana, tabla, entrada))
+
+# Función para botón seleccionar
+def seleccionar_producto(ventana, tabla, tabla_factura, entrada_producto, entrada_cantidad):
+    if int(entrada_cantidad.get()) <= 0:
+        messagebox.showerror('Error de cantidad', 'La cantidad seleccionada no puede ser menor a 1.', parent=ventana)
+        return
+    from .facturacion import actualizar_datos_facturación
+    info_producto = tabla.item(tabla.selection()[0], 'values')
+    productos_factura = get_productos_factura()
+    
+    producto = [info_producto[0], info_producto[1], info_producto[2], info_producto[3], int(entrada_cantidad.get()), info_producto[5], int(entrada_cantidad.get()) * float(info_producto[5])]
+    
+    if productos_factura:
+        producto_en_factura = False
+        for producto_factura in productos_factura:
+            if info_producto[0] in producto_factura:
+                producto_factura[4] += int(entrada_cantidad.get())
+                producto_factura[6] = float(producto_factura[5]) * int(producto_factura[4])
+                producto_en_factura = True
+                break
+        if not producto_en_factura:
+            productos_factura.append(producto)
+    else:
+        productos_factura.append(producto)
+    
+    set_productos_factura(productos_factura)
+    actualizar_datos_facturación(tabla_factura, True)
+    
+    entrada_producto.delete(0, tk.END)
+    entrada_producto.insert(0, "Buscar producto")
+    entrada_producto.config(fg='grey')
+    
+    for item in tabla.get_children():
+        tabla.delete(item)
